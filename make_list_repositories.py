092b6adc16691046963b151f6a8f142106286346
Ryui Kaneko <27846552+ryuikaneko@users.noreply.github.com>
2024-09-21 12:57:31 +0900

    update make_list_repositories.py

--- make_list_repositories.py
+++ make_list_repositories.py
@@ -22,3 +22,9 @@ for i in range(len(json)):
 ##  GHUSER=ryuikaneko; curl -s "https://api.github.com/users/$GHUSER/repos?per_page=1000" | grep -w clone_url | grep -o '[^"]\+://.\+.git'
 ##  GHUSER=ryuikaneko; curl -s "https://api.github.com/users/$GHUSER/repos?per_page=1000" | grep -w clone_url | grep -o '[^"]\+://.\+.git' | sed 's/^/- /g'
 ##  https://github.com/darshanc99/Python-Scraps-WT-purpose-/blob/master/git.py
+
+## [my memo]
+## - public only
+##   curl "https://api.github.com/users/ryuikaneko/repos?per_page=10000"
+## - private and public
+##   curl -H "Authorization: token [MYTOKEN]" "https://api.github.com/search/repositories?q=user:ryuikaneko&per_page=10000"
